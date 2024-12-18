bfd7f0557c6083b2200b2e65a96c2160874fd055
Lucas Yepez <lucasyepez99@gmail.com>
2024-12-17 21:15:45 -0600

    testing travis

--- deploy_render.sh
+++ deploy_render.sh
@@ -6,5 +6,5 @@ echo "Deploying to Render..."
 
 # Trigger the deployment by pushing to the Render-connected GitHub repository
 # Since Render is integrated with GitHub, pushing to the main branch should trigger an automatic deployment
-
+#
 git push render main  # Push to Render (ensure your GitHub repository is connected to Render)
