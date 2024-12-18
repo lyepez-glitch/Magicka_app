46eeed4107e8d7be5f9c8fa77a51a60458daa924
Lucas Yepez <lucasyepez99@gmail.com>
2024-12-17 19:29:53 -0600

    add to allowed hosts

--- magicka_app/settings.py
+++ magicka_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^ujmxr7f_m)&3r5xy=f3!=+^u4udxl@u8lfbn+p@%g%ajx68^^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','magicka-app.onrender.com']
 
 
 
