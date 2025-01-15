ac253b966c35a6cc47a1a227d7947fcf470ddf3b
Lucas Yepez <lucasyepez99@gmail.com>
2025-01-15 10:27:29 -0600

    added vercel

--- magicka_app/settings.py
+++ magicka_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^ujmxr7f_m)&3r5xy=f3!=+^u4udxl@u8lfbn+p@%g%ajx68^^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','magicka-app.onrender.com','magicka-frontend-qw6nzg1nc-lucas-projects-f61d5cb5.vercel.app','https://magicka-frontend-auth-2xdmua315-lucas-projects-f61d5cb5.vercel.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','magicka-app.onrender.com','https://magicka-frontend-auth-duh06in1h-lucas-projects-f61d5cb5.vercel.app']
 
 
 
@@ -93,9 +93,10 @@ TEMPLATES = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",'https://magicka-frontend-4gssgjfqd-lucas-projects-f61d5cb5.vercel.app',
-   'https://magicka-frontend-auth-2xdmua315-lucas-projects-f61d5cb5.vercel.app'
+    "http://localhost:5173",
+    "https://magicka-frontend-auth-duh06in1h-lucas-projects-f61d5cb5.vercel.app"
 ]
+
 
 WSGI_APPLICATION = 'magicka_app.wsgi.application'
 
