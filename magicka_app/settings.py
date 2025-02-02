5212be89b43cc4953409541bee55dd141d42050d
Lucas Yepez <lucasyepez99@gmail.com>
2025-02-01 21:17:16 -0600

    added allowed hosts

--- magicka_app/settings.py
+++ magicka_app/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 FRONTEND_URL = os.getenv('FRONTEND_URL')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',FRONTEND_URL,'https://magicka-app.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',FRONTEND_URL,os.getenv('BACKEND_URL')]
 
 
 
